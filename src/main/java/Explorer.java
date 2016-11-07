701fc7896e3884b1e9b7bb8a76a60daaea7b747c
Florian Lehmann <florian.lehmann@etu.unice.fr>
2016-11-07 16:01:57 +0100

    - Initial commit

---
+++ src/main/java/Explorer.java
@@ -0,0 +1,25 @@
+import eu.ace_design.island.bot.IExplorerRaid;
+
+
+public class Explorer implements IExplorerRaid {
+
+    @Override
+    public void initialize(String s) {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public String takeDecision() {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public void acknowledgeResults(String s) {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public String deliverFinalReport() {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+}
