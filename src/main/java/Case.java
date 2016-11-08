4290d2ca7cc6a0f603134b10cf783c816070e9f8
seb <kymine59@gmail.com>
2016-11-08 15:17:03 +0100

    seb: ajout constructeur Drone

--- src/main/java/Case.java
+++ src/main/java/Case.java
@@ -2,4 +2,5 @@
  * Created by seb on 08/11/16.
  */
 public class Case {
+
 }
--- src/main/java/Drone.java
+++ src/main/java/Drone.java
@@ -2,4 +2,9 @@
  * Created by seb on 08/11/16.
  */
 public class Drone {
+    private static String direction;
+
+    public Drone(String direction){
+        this.direction=direction;
+    }
 }
