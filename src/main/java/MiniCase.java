684463927d2f58e2a9a39646b476fc58cf610638
seb <kymine59@gmail.com>
2016-11-08 17:37:02 +0100

    seb: ajout de la classe Case + MiniCase

---
+++ src/main/java/MiniCase.java
@@ -0,0 +1,11 @@
+import java.awt.Point;
+
+/**
+ * Created by seb on 08/11/16.
+ */
+public class MiniCase extends Case {
+    public MiniCase(Point coords){
+        super.coords=coords;
+    }
+
+}
