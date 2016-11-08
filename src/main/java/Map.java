80df1de72ba553a4b5e3f118b59128a06168e191
seb <kymine59@gmail.com>
2016-11-08 19:12:29 +0100

    seb: ajout methoded Map

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -42,10 +42,81 @@ public class Map {
         }
         return null;
     }
-    public void setInfoCase(Point coord,String info){
+    public void setBiomeCase(Point coords,String biome){
+        for(int i=0;i<map.size();i++){
+            if(map.get(i).getCoords().equals(coords)){
+                map.get(i).setBiomes(biome);
+            }
+        }
 
     }
-    public void setInfoCase(Point coord, ArrayList<String> info){
+    public void setBiomeCase(Point coords,ArrayList<String> biomes){
+        for(int i=0;i<map.size();i++){
+            if(map.get(i).getCoords().equals(coords)){
+                map.get(i).setBiomes(biomes);
+            }
+        }
+    }
 
+    public void setRessourceCase(Point coords,String ressource){
+        for(int i=0;i<map.size();i++){
+            if(map.get(i).getCoords().equals(coords)){
+                map.get(i).setRessources(ressource);
+            }
+        }
+
+    }
+    public void setRessourceCase(Point coords,ArrayList<String> ressources){
+        for(int i=0;i<map.size();i++){
+            if(map.get(i).getCoords().equals(coords)){
+                map.get(i).setRessources(ressources);
+            }
+        }
+    }
+
+    public void setRessourcesQCase(Point coords,String quantity){
+        for(int i=0;i<map.size();i++){
+            if(map.get(i).getCoords().equals(coords)){
+                map.get(i).setRessources_quantity(quantity);
+            }
+        }
+
+    }
+    public void setRessourcesQCase(Point coords,ArrayList<String> ressourcesq){
+        for(int i=0;i<map.size();i++){
+            if(map.get(i).getCoords().equals(coords)){
+                map.get(i).setRessources(ressourcesq);
+            }
+        }
+    }
+
+
+    public void setCreek(Point coords){
+        for(int i=0;i<map.size();i++){
+            if(map.get(i).getCoords().equals(coords)){
+                map.get(i).creek();
+                break;
+            }
+        }
+    }
+
+    public Case getNearestCreek(){
+        List<Case> creeks=getcreek();
+        double distance=0;
+        Case nearest=null;
+        int a=getEmergency().getCoords().x;
+        int b=getEmergency().getCoords().y;
+        for(int i=0;i<creeks.size();i++){
+            int x=creeks.get(i).getCoords().x;
+            int y=creeks.get(i).getCoords().y;
+            double d=Math.sqrt((x-a)*(x-a)+(y-b)*(y-b));
+            if(d<distance){
+                distance=d;
+                nearest=creeks.get(i);
+            }
+
+
+        }
+        return nearest;
     }
 }
