ea82a4bab99acbb4a1305678d26de25f69fc4273
Florian Lehmann <florian.lehmann@etu.unice.fr>
2016-11-11 23:15:25 +0100

    -ajout des méthodes initialize et takedecision

--- src/main/java/Explorer.java
+++ src/main/java/Explorer.java
@@ -1,24 +1,147 @@
 import eu.ace_design.island.bot.IExplorerRaid;
-
+import org.json.*;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
 
 public class Explorer implements IExplorerRaid {
 
+    static private int men;
+    static private List<Men> menList;
+    static private int budget;
+    static private String heading;
+    static private Map<String, Integer> contracts;
+    static private String resultat;
+    private String action;
+    private int cost;
+    private int range;
+    private String found;
+    private boolean status;
+    private Drone drone;
+    private int etat;
     @Override
-    public void initialize(String s) { throw new UnsupportedOperationException("Not yet implemented!"); }
+    public void initialize(String s) {
+	resultat = new String();
+	action = new String();
+	cost = 0;
+	etat = 0;
+	range = 0;
+	found = new String();
+	status = true;// est-ce que le staus est verifie
+	drone = new Drone();
+	
+	JSONObject jsonobject = new JSONObject(s);
+	if (jsonobject.has("men"))
+	{
+	    this.men = jsonobject.getInt("men");
+	    menList = new LinkedList<Men>();
+	    for( int i = 0; i < men; i++)
+		menList.add(new Men());
+	}
+	if (jsonobject.has("budget"))
+	    this.budget = jsonobject.getInt("budget");
+	if (jsonobject.has("heading"))
+	    this.heading = jsonobject.getString("heading");
+	if (jsonobject.has("contracts"))
+	    {
+		String tmp;
+		JSONArray array = jsonobject.getJSONArray("contracts");
+		JSONObject jsonobj = new JSONObject();
+		contracts = new HashMap<String, Integer>();
+		Iterator<String> iterator;
+		for( int i = 0; i < array.length() ;i++){
+		    jsonobj = array.getJSONObject(i);
+		    iterator = jsonobj.keys();
+		    while( iterator.hasNext() ){
+			tmp = iterator.next();
+			contracts.put(jsonobj.getString(iterator.next()), jsonobj.getInt(tmp));
+		    }
+		}
+
+	    }
+				  
+    }
 
     @Override
     public String takeDecision() {
-        throw new UnsupportedOperationException("Not yet implemented!");
+	switch(etat){
+
+	case 0:
+	    if( !findIsland() )
+		etat++;
+	    else
+		this.action = drone.getAction();		
+	    break;
+	case 1:
+	    this.action = "{ \"action\": \"stop\" }";
+		break;
+	default:
+	    this.action = "{ \"action\": \"stop\" }";
+		break;
+	    
+
+	}
+	return this.action;
     }
 
     @Override
     public void acknowledgeResults(String s) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+	JSONObject jsonobject = new JSONObject(s);
+	switch(action)
+	    {
+	    case "ECHO":
+		if( jsonobject.has("cost"))
+		    this.cost = jsonobject.getInt("cost");
+		if( jsonobject.has("extras"))
+		    {
+			JSONArray array = jsonobject.getJSONArray("extras");
+			range = array.getInt(0);
+			found = array.getString(1);
+		    }
+
+		    if ( jsonobject.has("status"))
+			this.status = jsonobject.getString("status").equals("OK");
+
+
+		    drone.setResult(found);
+		    drone.setNbCase(range);
+
+		break;
+		//rajouter LE SCAN
+
+	    }
+
     }
 
     @Override
     public String deliverFinalReport() {
         throw new UnsupportedOperationException("Not yet implemented!");
     }
-
+    static public String getResult()
+    {
+	return resultat;
+    }
+    static public int getNumberMen()
+    {
+	return men;
+    }
+    static public int getBudget()
+    {
+	return budget;
+    }
+    static public String getHeading()
+    {
+	return heading;
+    }
+    static public List<Men> getMens()
+    {
+	return menList;
+    }
+    static public Map<String, Integer> getContracts()
+    {
+	return contracts;
+    }
+	
 }
---
+++ src/test/java/ExplorerTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Map;
+import java.util.HashMap;
+import static org.junit.Assert.*;
+
+public class ExplorerTest{
+
+    Explorer explorer;
+
+    @Before
+    public void defineContext(){
+	explorer = new Explorer();
+	String str = "{\"men\": 12,\"budget\": 10000,\"contracts\": [{ \"amount\": 600, \"resource\": \"WOOD\" },{ \"amount\": 200, \"resource\": \"GLASS\" }],\"heading\": \"W\"}";
+	    explorer.initialize(str);
+    }
+
+    @Test
+    public void checkJson(){
+	assertEquals(12, Explorer.getNumberMen());
+    }
+
+    @Test
+    public void checkJson2(){
+	assertEquals(10000, Explorer.getBudget());
+    }
+
+    @Test
+    public void checkJson3(){
+	assertEquals("W", Explorer.getHeading());
+    }
+
+    @Test
+    public void checkJson4(){
+	Map<String, Integer> test = new HashMap();
+	test = Explorer.getContracts();
+	int wood = test.get("WOOD");
+	int glass = test.get("GLASS");
+	assertTrue(wood == 600 && glass == 200);
+    }
+
+    
+}
