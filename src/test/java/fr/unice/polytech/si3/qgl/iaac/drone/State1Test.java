73d85067d0d301dd62a80f46e02dfe4e7fa729ab
Florian Lehmann <florian.lehmann@etu.unice.fr>
2016-12-05 17:14:45 +0100

    modif de test

--- src/test/java/fr/unice/polytech/si3/qgl/iaac/drone/State1Test.java
+++ src/test/java/fr/unice/polytech/si3/qgl/iaac/drone/State1Test.java
@@ -1,11 +1,12 @@
 package fr.unice.polytech.si3.qgl.iaac.drone;
 
-//import fr.unice.polytech.si3.qgl.iaac.drone.Drone;
+import fr.unice.polytech.si3.qgl.iaac.drone.Drone.*;
 import static fr.unice.polytech.si3.qgl.iaac.EnumDirection.*;
 import static fr.unice.polytech.si3.qgl.iaac.EnumJSON.*;
 import fr.unice.polytech.si3.qgl.iaac.EnumDirection;
 import fr.unice.polytech.si3.qgl.iaac.carte.Carte;
 import org.junit.*;
+import static org.junit.Assert.*;
 
 public class State1Test {
 
@@ -19,12 +20,14 @@ public class State1Test {
 		
     }
 
-    /*@Test
+    @Ignore
     public void ActionTest() {
+	init();
 	drone.setState(new State1());
-	drone.getState().execute();
+	drone.getState().execute(drone);
 	assertEquals(ECHO.toString(drone.getDirection().front()), drone.getAction());
-    }*/
+	//assertTrue(true);
+    }
     
 
 }
