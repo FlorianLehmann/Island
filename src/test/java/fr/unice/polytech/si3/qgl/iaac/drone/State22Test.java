86e41d131fb25c74fc919a45f2daff018bba2c7b
dq610272 <quentin.duret@etu.unice.fr>
2016-12-06 16:11:39 +0100

    création test

---
+++ src/test/java/fr/unice/polytech/si3/qgl/iaac/drone/State22Test.java
@@ -0,0 +1,36 @@
+package fr.unice.polytech.si3.qgl.iaac.drone;
+
+import static fr.unice.polytech.si3.qgl.iaac.EnumDirection.*;
+import static fr.unice.polytech.si3.qgl.iaac.EnumJSON.*;
+import static org.junit.Assert.assertEquals;
+
+
+import fr.unice.polytech.si3.qgl.iaac.EnumDirection;
+import fr.unice.polytech.si3.qgl.iaac.ReadJSON;
+import fr.unice.polytech.si3.qgl.iaac.carte.Carte;
+import org.junit.*;
+/**
+ * Created by Quentin on 06/12/2016.
+ */
+public class State22Test {
+
+    private ReadJSON read;
+    private Drone drone;
+    private Carte carte;
+
+    @Before
+    public void init() {
+        read = new ReadJSON();
+        read.read("{\"men\": 12,\"budget\": 10000,\"contracts\": [{ \"amount\": 600, \"resource\": \"WOOD\" },{ \"amount\": 200, \"resource\": \"GLASS\" }],\"heading\": \"W\"}");
+        carte = new Carte();
+        drone = new Drone(NORD, carte);
+
+    }
+
+    @Test
+    public void ActionTest() {
+        drone.setState(new State22());
+        drone.getState().execute(drone);
+        assertEquals(FLY.toString(""), drone.getAction());
+    }
+}
