b92bbce369f36fdb1c1046a0207bb0849df29bfb
dq610272 <quentin.duret@etu.unice.fr>
2017-02-04 17:08:23 +0100

    Drone and EnumJson

---
+++ src/main/java/fr/unice/polytech/si3/qgl/iaac/EnumJSON.java
@@ -0,0 +1,56 @@
+package fr.unice.polytech.si3.qgl.iaac;
+
+/**
+ * Created by Quentin on 03/02/2017.
+ */
+public enum EnumJSON {
+
+    SCAN("{ \"action\": \"scan\" }", "", ""),
+    ECHO("{ \"action\": \"echo\", \"parameters\": { \"direction\":\"", "", "\" } }"),
+    FLY("{ \"action\": \"fly\" }", "", ""),
+    STOP("{ \"action\": \"stop\" }", "", ""),
+    HEADING("{ \"action\": \"heading\", \"parameters\": { \"direction\":\"", "", "\" } }"),
+    LAND("{ \"action\": \"land\", \"parameters\": { \"creek\": \"", "\", \"people\": ", " }}"),
+    MOVETO("{ \"action\": \"move_to\", \"parameters\": { \"direction\": \"", "", "\" } }"),
+    SCOUT("{ \"action\": \"scout\", \"parameters\": { \"direction\": \"", "", "\" } }"),
+    GLIMPSE("{ \"action\": \"glimpse\", \"parameters\": { \"direction\": \"", "\", \"range\": ", " } }"),
+    EXPLORE("{ \"action\": \"explore\" }", "", ""),
+    EXPLOIT("{ \"action\": \"exploit\", \"parameters\": { \"resource\": \"", "", "\" }}");
+
+    String debut, milieu, fin;
+
+
+    /**
+     * default constructor
+     * @param debut
+     * @param milieu
+     * @param fin
+     */
+    EnumJSON(String debut, String milieu, String fin) {
+        this.debut = debut;
+        this.fin = fin;
+        this.milieu = milieu;
+    }
+
+    /**
+     * Retourne la requete JSON en String
+     *
+     * @return String
+     */
+    public String toString(String direction) {
+
+        return debut + direction + fin;
+
+    }
+
+    /**
+     *
+     * @param parametre1
+     * @param parametre2
+     * @return
+     */
+    public String toString(String parametre1, int parametre2) {
+        return debut + parametre1 + milieu + parametre2 + fin;
+    }
+
+}
---
+++ src/main/java/fr/unice/polytech/si3/qgl/iaac/airPhase/Drone.java
@@ -0,0 +1,68 @@
+package fr.unice.polytech.si3.qgl.iaac.airPhase;
+
+import fr.unice.polytech.si3.qgl.iaac.EnumDirection;
+import fr.unice.polytech.si3.qgl.iaac.EnumOrientation;
+
+import java.awt.*;
+
+import static fr.unice.polytech.si3.qgl.iaac.EnumJSON.*;
+
+/**
+ * Created by Quentin on 03/02/2017.
+ */
+public class Drone {
+
+    private Point coord;
+    private EnumOrientation orientation;
+
+    public Drone(Point coord, EnumOrientation orientation) {
+        this.coord = coord;
+        this.orientation = orientation;
+    }
+
+
+    public String echo(EnumDirection direction){
+        orientation = direction.getDirection(orientation);
+        return ECHO.toString(orientation.toString());
+    }
+
+
+    public String scan(){
+        return SCAN.toString();
+    }
+
+
+    public String fly(){
+        this.changeCoord();
+        return FLY.toString(orientation.direction());
+    }
+
+
+    public String heading(EnumDirection direction){
+        this.changeCoord();
+        orientation = direction.getDirection(orientation);
+        this.changeCoord();
+        return HEADING.toString(orientation.direction());
+    }
+
+
+    public void changeCoord(){
+        switch (orientation) {
+            case NORTH:
+                coord.setLocation(coord.getX(), coord.getY() + 1);
+                break;
+            case SOUTH:
+                coord.setLocation(coord.getX(), coord.getY() - 1);
+                break;
+            case WEST:
+                coord.setLocation(coord.getX() - 1, coord.getY());
+                break;
+            case EST:
+                coord.setLocation(coord.getX() + 1, coord.getY());
+                break;
+            default:
+                break;
+        }
+    }
+
+}
