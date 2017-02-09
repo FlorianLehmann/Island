8b394fac42b371ed60f6fe96a03ff341d3dbd163
LehmannF <florian.lehmann@etu.unice.fr>
2017-02-09 11:35:02 +0100

    Ajout des enums

---
+++ src/main/java/fr/unice/polytech/si3/qgl/iaac/resources/EnumPrimaryResources.java
@@ -0,0 +1,28 @@
+package fr.unice.polytech.si3.qgl.iaac.resources;
+
+/**
+ * Created by florian on 09/02/2017.
+ */
+public enum EnumPrimaryResources implements EnumResources {
+
+    FISH("FISH"),
+    FLOWER("FLOWER"),
+    FRUITS("FRUITS"),
+    FUR("FUR"),
+    ORE("ORE"),
+    QUARTZ("QUARTZ"),
+    SUGAR_CANE("SUGAR_CANE"),
+    WOOD("WOOD");
+
+    String name;
+
+    EnumPrimaryResources(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
