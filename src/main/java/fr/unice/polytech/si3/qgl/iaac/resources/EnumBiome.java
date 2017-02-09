ba60cf6eb4f3d682b36b06be72d2b43aae65bd2e
dq610272 <quentin.duret@etu.unice.fr>
2017-02-09 12:10:06 +0100

    EnumBiome

---
+++ src/main/java/fr/unice/polytech/si3/qgl/iaac/resources/EnumBiome.java
@@ -0,0 +1,47 @@
+package fr.unice.polytech.si3.qgl.iaac.resources;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static fr.unice.polytech.si3.qgl.iaac.resources.EnumPrimaryResources.*;
+
+/**
+ * Created by Quentin on 09/02/2017.
+ */
+public enum EnumBiome {
+
+
+
+    OCEAN(FISH),
+    LAKE(FISH),
+    BEACH(QUARTZ),
+    GRASSLAND(FUR),
+    MANGROVE(FLOWER,WOOD),
+    TROPICAL_RAIN_FOREST(FRUITS,SUGAR_CANE,WOOD),
+    TROPICAL_SEASONAL_FOREST(FRUITS,SUGAR_CANE,WOOD),
+    TEMPERATE_DECIDUOUS_FOREST(WOOD),
+    TEMPERATE_RAIN_FOREST(FUR,WOOD),
+    TEMPERATE_DESERT(ORE,QUARTZ),
+    TAIGA(WOOD),
+    SNOW(),
+    TUNDRA(FUR),
+    ALPINE(FLOWER,ORE),
+    GLACIER(FLOWER,ORE),
+    SHRUBLAND(FUR),
+    SUB_TROPICAL_DESERT(ORE,QUARTZ);
+
+    List<EnumResources> resources;
+
+    EnumBiome (EnumResources ... resources){
+        this.resources = new ArrayList<>();
+        for(EnumResources resource : resources){
+            this.resources.add(resource);
+        }
+    }
+
+    public List<EnumResources> getResources(){
+        return resources;
+    }
+}
