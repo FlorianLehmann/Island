90003a4489a252b2e89afba5cbf6124b5cd0b660
dq610272 <quentin.duret@etu.unice.fr>
2017-02-19 15:16:31 +0100

    menTest

---
+++ src/test/java/fr/unice/polytech/si3/qgl/iaac/ground/MenTest.java
@@ -0,0 +1,69 @@
+package fr.unice.polytech.si3.qgl.iaac.ground;
+
+import fr.unice.polytech.si3.qgl.iaac.Ground.Men;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static fr.unice.polytech.si3.qgl.iaac.EnumJSON.EXPLOIT;
+import static fr.unice.polytech.si3.qgl.iaac.EnumJSON.EXPLORE;
+import static fr.unice.polytech.si3.qgl.iaac.EnumJSON.MOVETO;
+import static fr.unice.polytech.si3.qgl.iaac.EnumOrientation.*;
+import static fr.unice.polytech.si3.qgl.iaac.resources.EnumPrimaryResources.WOOD;
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by Quentin on 19/02/2017.
+ */
+public class MenTest {
+
+    Men men;
+
+    @Before
+    public void ini(){
+        Point coords = new Point(3,3);
+        men = new Men(coords);
+    }
+
+    @Test
+    public void moveToNorth(){
+        Point point = new Point(3,4);
+        assertEquals(men.moveTo(NORTH), MOVETO.toString(NORTH.front().toString()));
+        assertEquals(men.getCoord(),point);
+    }
+
+    @Test
+    public void moveToSouth(){
+        Point point = new Point(3,2);
+        assertEquals(men.moveTo(SOUTH), MOVETO.toString(SOUTH.front().toString()));
+        assertEquals(men.getCoord(),point);
+    }
+
+    @Test
+    public void moveToEST(){
+        Point point = new Point(4,3);
+        assertEquals(men.moveTo(EST), MOVETO.toString(EST.front().toString()));
+        assertEquals(men.getCoord(),point);
+
+    }
+
+    @Test
+    public void moveToWest(){
+        Point point = new Point(2,3);
+        assertEquals(men.moveTo(WEST), MOVETO.toString(WEST.front().toString()));
+        assertEquals(men.getCoord(),point);
+
+    }
+
+    @Test
+    public void exploreTest(){
+        assertEquals(men.explore(), EXPLORE.toString(""));
+    }
+
+    @Test
+    public void exploitWood(){
+        assertEquals(men.exploit(WOOD), EXPLOIT.toString(WOOD.toString()));
+    }
+
+}
