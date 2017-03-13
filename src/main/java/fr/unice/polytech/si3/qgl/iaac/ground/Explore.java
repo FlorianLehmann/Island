bb4e249b4c412bee4ba08a3dc7d06cd8c3cbffdc
dq610272 <quentin.duret@etu.unice.fr>
2017-03-13 15:13:49 +0100

    IAAC-39 ramasse toutes les ressources sur la case qui correspond à un contrat

--- src/main/java/fr/unice/polytech/si3/qgl/iaac/ground/Explore.java
+++ src/main/java/fr/unice/polytech/si3/qgl/iaac/ground/Explore.java
@@ -4,6 +4,13 @@ import fr.unice.polytech.si3.qgl.iaac.carte.Carte;
 import fr.unice.polytech.si3.qgl.iaac.contracts.Contract;
 import fr.unice.polytech.si3.qgl.iaac.contracts.Contracts;
 import fr.unice.polytech.si3.qgl.iaac.ReadJSON;
+import fr.unice.polytech.si3.qgl.iaac.resources.EnumPrimaryResources;
+import fr.unice.polytech.si3.qgl.iaac.resources.EnumResources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static fr.unice.polytech.si3.qgl.iaac.resources.EnumPrimaryResources.getEnumPrimaryResources;
 
 /**
  * Created by lehmann on 12/02/17.
@@ -11,10 +18,12 @@ import fr.unice.polytech.si3.qgl.iaac.ReadJSON;
 public class Explore implements State {
 
     private Contract contract;
+    private Contracts contracts;
 
     @Override
     public String execute(Men men, Contracts contracts, Carte carte) {
         contract =  contracts.getPrimaryContract();
+        this.contracts = contracts;
         return men.explore();
     }
 
@@ -22,14 +31,19 @@ public class Explore implements State {
     @Override
     public State wait(ReadJSON json) {
         boolean resource;
+        List<EnumResources> resources = new ArrayList<>();
         resource = false;
         for (int i = 0; i < json.getResources().size() ; i++) {
             if(json.getResources().get(i).equals(contract.getName().toString()))
                 resource = true;
+            //TODO retourner des primary de JSON
+            if(contracts.containRessource((EnumPrimaryResources)(getEnumPrimaryResources(json.getResources().get(i))))){
+                resources.add(getEnumPrimaryResources(json.getResources().get(i)));
+            }
         }
 
         if (resource)
-            return new Exploit();
+            return new Exploit(resources);
         else
             return new DefineWay();
             //return new TounerRond();
