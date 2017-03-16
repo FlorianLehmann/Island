6d171ec2e606aab85878db208234aa01f0d588c3
LehmannF <florian.lehmann@etu.unice.fr>
2017-03-16 13:24:02 +0100

    IAAC-39

--- src/main/java/fr/unice/polytech/si3/qgl/iaac/carte/ArrayMap.java
+++ src/main/java/fr/unice/polytech/si3/qgl/iaac/carte/ArrayMap.java
@@ -24,6 +24,8 @@ public class ArrayMap {
     private Map<Point, Case> map;
     private Map<Point, Boolean> edge;
     private List<Point> listeY;
+    Map<Point, Boolean> edge_tmp;
+
 
     private int xMax;
     private int xMin;
@@ -36,6 +38,7 @@ public class ArrayMap {
     private static final Logger logger = LogManager.getLogger(ArrayMap.class);
 
     public ArrayMap(Map<Point, Case> map) {
+        edge_tmp = new HashMap<>();
         this.map = map;
         edge = new HashMap<>();
         listeY = new LinkedList<>();
@@ -94,30 +97,42 @@ public class ArrayMap {
             if (!tile.getValue().containOcean())
                 edge.put(tile.getValue().getCoords() ,isAnEdge(tile.getValue().getCoords()));
 
-        for (Map.Entry<Point, Boolean> tile: edge.entrySet())
-            logger.info(tile.getKey());
+        //for (Map.Entry<Point, Boolean> tile: edge.entrySet())
+//            logger.info(strto());
 
         defineMapSize();
 
         for (Map.Entry<Point, Boolean> tile: edge.entrySet()) {
             if (left(edge, tile.getKey()) && !right(edge, tile.getKey())) {
                 // droite + 3
-                Point point = new Point(tile.getKey().x + 3, tile.getKey().y);
+                Point point = new Point(tile.getKey().x + 4, tile.getKey().y);
                 // on passe sur toute la ligne
                 if (hasEdgeMinusY(tile.getKey())) {
+
                     for (int i = yMax; i >= yMin ; i--) {
                         //si on trouve on relie
-                        if (edge.get(new Point(point.x, i))) {
-                            connect(new Point(point.x, i), point);
+
+                        if (edge.getOrDefault(new Point(point.x, i), false)) {
+                            logger.info("OKOKOKOKOKOKOKOK");
+
+                            connectX(new Point(point.x, i), point);//TODO
+                            //TODO METTRE UN BREAK
+                            //TODO METTRE UN BREAK
+                            //TODO METTRE UN BREAK
+                            //TODO METTRE UN BREAK
+                            //TODO METTRE UN BREAK
                         }
                         //si on trouve on relie
                     }
                 }
                 else {
+
+
                     for (int i = yMin; i <= yMax ; i++) {
                         //si on trouve on relie
-                        if (edge.get(new Point(point.x, i))) {
-                            connect(new Point(point.x, i), point);
+                        if (edge.getOrDefault(new Point(point.x, i), false)) {
+
+                            connectX(new Point(point.x, i), point);
                         }
 
                     }
@@ -126,24 +141,143 @@ public class ArrayMap {
 
             }
             else if (right(edge, tile.getKey()) && !left(edge, tile.getKey())) {
+                Point point = new Point(tile.getKey().x - 4, tile.getKey().y);
 
+                if (hasEdgeMinusY(tile.getKey())) {
+                    for (int i = yMax; i >= yMin ; i--) {
+                        //si on trouve on relie
+                        if (edge.getOrDefault(new Point(point.x, i), false)) {
+
+                            connectX(new Point(point.x, i), point);
+                        }
+                        //si on trouve on relie
+                    }
+                }
+                else {
+                    for (int i = yMin; i <= yMax ; i++) {
+                        //si on trouve on relie
+                        if (edge.getOrDefault(new Point(point.x, i), false)) {
+
+                            connectX(new Point(point.x, i), point);
+                        }
+
+                    }
+                }
             }
             else if (up(edge, tile.getKey()) && !down(edge, tile.getKey())) {
+                Point point = new Point(tile.getKey().x , tile.getKey().y - 4 );//todo
+
+                if (hasEdgeMinusX(tile.getKey())) {
+                    for (int i = xMax; i >= xMin ; i--) {
+                        //si on trouve on relie
+                        if (edge.getOrDefault(new Point(i, point.y), false)) {
+
+                            connectY(new Point(i, point.y), point);
+                        }
+                        //si on trouve on relie
+                    }
+                }
+                else {
+                    for (int i = xMin; i <= xMax ; i++) {
+                        //si on trouve on relie
+                        if (edge.getOrDefault(new Point(i, point.y), false)) {
+
+                            connectY(new Point(i, point.y), point);
+                        }
+                    }
+                }
+            }
+            else if (down(edge, tile.getKey()) && !up(edge, tile.getKey())){//todo
+                Point point = new Point(tile.getKey().x , tile.getKey().y + 4 );//todo
+
+                if (hasEdgeMinusX(tile.getKey())) {
+                    for (int i = xMax; i >= xMin ; i--) {
+                        //si on trouve on relie
+                        if (edge.getOrDefault(new Point(i, point.y), false)) {
+
+                            connectY(new Point(i, point.y), point);
+                        }
+                        //si on trouve on relie
+                    }
+                }
+                else {
+                    for (int i = xMin; i <= xMax ; i++) {
+                        //si on trouve on relie
+                        if (edge.getOrDefault(new Point(i, point.y), false)) {
+
+                            connectY(new Point(i, point.y), point);
+                        }
+                    }
+                }
+
 
             }
-            else if (down(edge, tile.getKey()) && !up(edge, tile.getKey())){
+        }
 
+        for (Map.Entry<Point, Boolean> tile: edge_tmp.entrySet())
+            edge.put(tile.getKey(), tile.getValue());
+
+        logger.info(strto());
+
+    }
+
+    private boolean hasEdgeMinusX(Point key) { //todo
+        for (int i = key.x -1 ; i >= xMin ; i--) {
+            if (edge.getOrDefault(new Point(i,key.y), false)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void connectX(Point point1, Point point) {
+        //équation de la droite
+        //vecteur
+        int a = point.x - point1.x;
+        int b = point.y - point1.y;
+        int c = -a*point.x + b * point.y;
+
+        if (point.x < point1.x) {
+            for (int x = point.x; x < point1.x; x++) {
+                Point p = new Point(x, (int) ((a * x + c) / b));
+                edge_tmp.put(p, true);
+            }
+        }
+        else {
+            for (int x = point1.x; x < point.x; x++) {
+                Point p = new Point(x, (int) ((a * x + c) / b));
+                edge_tmp.put(p, true);
             }
         }
 
     }
 
-    private void connect(Point point, Point point1) {
+    private void connectY(Point point1, Point point) {
+        //équation de la droite
+        //vecteur
+        int a = point.x - point1.x;
+        int b = point.y - point1.y;
+        int c = -a*point.x + b * point.y;
+
+        if (point.y < point1.y) {
+            for (int y = point.y; y < point1.y; y++) {
+                Point p = new Point((int) ((b*y-c)/a), y);
+                edge_tmp.put(p, true);
+            }
+        }
+        else {
+            for (int y = point1.y; y < point.y; y++) {
+                Point p = new Point((int) ((b*y-c)/a), y);
+                edge_tmp.put(p, true);
+            }
+        }
+        //A*x - b*y + c =0
+        //(b*y-c)/a
     }
 
     private boolean hasEdgeMinusY(Point key) {
         for (int i = key.y -1 ; i >= yMin ; i--) {
-            if (edge.get(new Point(key.x,i))) {
+            if (edge.getOrDefault(new Point(key.x,i), false)) {
                 return true;
             }
         }
@@ -151,19 +285,19 @@ public class ArrayMap {
     }
 
     private boolean up(Map<Point, Boolean> edge, Point key) {
-        return edge.get(new Point(key.x, key.y + 1));
+        return edge.getOrDefault(new Point(key.x, key.y + 1),false);
     }
 
     private boolean down(Map<Point, Boolean> edge, Point key) {
-        return edge.get(new Point(key.x, key.y - 1));
+        return edge.getOrDefault(new Point(key.x, key.y - 1),false);
     }
 
     private boolean left(Map<Point, Boolean> edge, Point key) {
-        return edge.get(new Point(key.x - 1, key.y));
+        return edge.getOrDefault(new Point(key.x - 1, key.y),false);
     }
 
     private boolean right(Map<Point, Boolean> edge, Point key) {
-        return edge.get(new Point(key.x + 1, key.y));
+        return edge.getOrDefault(new Point(key.x + 1, key.y),false);
     }
 
     public boolean isEdge(Point point) {
